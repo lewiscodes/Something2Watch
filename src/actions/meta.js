9a07532f44a95b1783c65d1a00e41bbe3ac68869
Lewis <lewis.turner@lewiss-air.home>
2018-07-23 22:06:04 +0100

    added function to get genres from results_api

--- src/actions/meta.js
+++ src/actions/meta.js
@@ -1,8 +1,23 @@
 export const SET_SEARCH_TYPE = "SET_SEARCH_TYPE";
+export const GET_GENRES = "GET_GENRES";
+export const RECEIVE_GENRES = "RECEIVE_GENRES";
 
 export function setSearchType(searchType) {
   return {
     type: SET_SEARCH_TYPE,
     payload: searchType
+  }
+}
+
+export function getGenres(searchString) {
+  return dispatch => {
+    dispatch({type: GET_GENRES})
+
+    return window.fetch(searchString).then(response => response.json()).then((results) => {
+      dispatch({
+        type: RECEIVE_GENRES,
+        payload: results
+      })
+    });
   }
 }
--- src/reducers/reducer_meta.js
+++ src/reducers/reducer_meta.js
@@ -1,12 +1,14 @@
 import settings from '../settings.json'
-import { SET_SEARCH_TYPE } from '../actions/meta';
+import { SET_SEARCH_TYPE, RECEIVE_GENRES } from '../actions/meta';
 
-const INITIAL_STATE = { api: settings.api, searchType: '' };
+const INITIAL_STATE = { api: settings.api, resultsApi:settings.resultsApi, searchType: '', genres: null };
 
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case SET_SEARCH_TYPE:
       return {...state, searchType: action.payload}
+    case RECEIVE_GENRES:
+      return {...state, genres: action.payload.genres}
     default:
       return state;
   }
