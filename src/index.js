7054343de2fb321389f7a051c078df2e1176c65b
Lewis Turner <lewis.turner@Lewiss-MacBook-Air.local>
2018-10-01 11:13:57 +0100

    fixed linting errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducers from './reducers';
-import promise from 'redux-promise';
-import thunk from 'redux-thunk';
-import registerServiceWorker from './registerServiceWorker';
-import { StyleRoot } from 'radium';
-import routes from './routes.js';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducers from './reducers'
+import promise from 'redux-promise'
+import thunk from 'redux-thunk'
+import registerServiceWorker from './registerServiceWorker'
+import { StyleRoot } from 'radium'
+import routes from './routes.js'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const createStoreWithMiddleware = createStore(reducers, composeEnhancers(
   applyMiddleware(promise, thunk)
-));
+))
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware}>
@@ -21,5 +21,5 @@ ReactDOM.render(
       <Router history={browserHistory} routes={routes} />
     </StyleRoot>
   </Provider>
-, document.getElementById('root'));
-registerServiceWorker();
+  , document.getElementById('root'))
+registerServiceWorker()
